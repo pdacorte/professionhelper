a26580f8f0076f2facc9104de753c70fa6c57ec8
greenteapls <blackwaterp@hotmail.com>
2025-01-12 16:37:27 +0100

    Updated the dailies app code

--- dailiesapp/dailies.py
+++ dailiesapp/dailies.py
@@ -61,7 +61,6 @@ def main():
 
         result = db.select_all()
         df = pd.DataFrame(result, columns=['Task', 'Type', 'Status', 'Start Date', 'Finished Date'])
-        
         st.dataframe(df)
 
         with st.expander("Statistics"):
@@ -77,8 +76,80 @@ def main():
 
     elif choice == "Update":
         st.subheader("Edit/Update Items")
+
+        result = db.select_all()
+        df = pd.DataFrame(result, columns=['Task', 'Type', 'Status', 'Start Date', 'Finished Date'])
+        
+        st.dataframe(df)
+        list_of_tasks = [i[0] for i in db.select_unique()]
+        st.write(list_of_tasks)
+
+        selected_task = st.selectbox("Task to Edit", list_of_tasks)
+
+        selected_result = db.return_task(selected_task)
+
+
+        # Update Part
+        if selected_result:
+            task = selected_result[0][0]
+            task_type = selected_result[0][1]
+            task_status = selected_result[0][2]
+            task_start_date = selected_result[0][3]
+
+
+            col1,col2 = st.columns(2)
+
+            with col1:
+                updated_task = st.text_input("Task to Do", task)
+                if task_type == "Goal":
+                    type_index = 0
+                else:
+                    type_index = 1
+                updated_task_type = st.selectbox("Task Type", ["Goal", "Non-Negotiable"], index=type_index)
+
+            with col2:
+                if task_status == "Not done":
+                    status_index = 0
+                else:
+                    status_index = 1
+                updated_task_status = st.selectbox("Task Status", ["Not done", "Done"], index=status_index)
+                updated_task_start_date = st.date_input("Start Date",value=task_start_date)
+
+            if st.button("Update Task"):
+                db.update_row(updated_task, updated_task_type, updated_task_status, updated_task_start_date, task, task_type, task_status, task_start_date)
+                st.success("Successfully updated task: {} to {}".format(task,updated_task))
+
+            with st.expander("Updated Data"):
+                result2 = db.select_all()
+                df2 = pd.DataFrame(result2, columns=['Task', 'Type', 'Status', 'Start Date', 'Finished Date'])
+                st.dataframe(df2)
+
+
+
+
     elif choice == "Delete":
         st.subheader("Delete Item")
+
+        with st.expander("Current Data"):
+                result2 = db.select_all()
+                df2 = pd.DataFrame(result2, columns=['Task', 'Type', 'Status', 'Start Date', 'Finished Date'])
+                st.dataframe(df2)
+        
+        
+        list_of_tasks = [i[0] for i in db.select_unique()]
+        selected_task = st.selectbox("Task to Delete", list_of_tasks)
+        st.warning("Do you want to delete {}?".format(selected_task))
+
+        if st.button("Delete Task"):
+            db.delete_row(selected_task)
+            st.success("Successfully deleted task: {}".format(selected_task))
+
+
+
+
+
+
+
     else:
         st.subheader("About")
 
--- dailiesapp/dailiesdb.py
+++ dailiesapp/dailiesdb.py
@@ -16,9 +16,42 @@ def create_row(task, task_type, task_status, task_start_date):
 
 def select_all():
     c.execute('SELECT * FROM tasktable')
+    # Keep data post execution
     data = c.fetchall()
     return data
 
+def select_unique():
+    c.execute("SELECT DISTINCT task FROM tasktable")
+    data = c.fetchall()
+    return data
+
+def return_task(task):
+    c.execute("SELECT * FROM tasktable WHERE task='{}'".format(task))
+    data = c.fetchall()
+    return data
+
+def update_row(updated_task, updated_task_type, updated_task_status, updated_task_start_date, task, task_type, task_status, task_start_date):
+    c.execute('UPDATE tasktable SET task=?, task_type=?, task_status=?, task_start_date=? WHERE task=? AND task_type=? AND task_status=? AND task_start_date=?', (updated_task, updated_task_type, updated_task_status, updated_task_start_date, task, task_type, task_status, task_start_date))
+    conn.commit()
+
+def delete_row(task):
+    c.execute('DELETE FROM tasktable WHERE task="{}"'.format(task))
+    conn.commit()
+
+# GOTTA FIGURE THIS OUT LATAER
+def completion_date():
+    c.execute("""
+CREATE TRIGGER IF NOT EXISTS update_task_end_date
+AFTER UPDATE OF task_status ON tasktable
+FOR EACH ROW
+WHEN NEW.task_status = 'Done' AND OLD.task_status = 'Not done'
+BEGIN
+    UPDATE tasks
+    SET task_finish_date = DATE('now')
+    WHERE task_id = NEW.task_id;
+END;
+""")
+    c.commit()  # Save changes
 #"""
 #Type - Choice - Non-Negotiable/Daily
 #Goal - Text Field 
